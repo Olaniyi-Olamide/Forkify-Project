a4b96b183f61c5059e322cd47cbdeaa8ba50d719
Olaniyi Olamide <olamideolaniyi616@gmail.com>
2025-09-24 14:03:32 +0100

    added and modified config and helpers file

--- src/js/model.js
+++ src/js/model.js
@@ -1,17 +1,13 @@
-// import { async } from './regenerator-runtime';
-
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
+    const data = await getJSON(`${API_URL}/${id}`);
 
-    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -26,6 +22,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    console.error(err);
+    console.error(`my err: ${err}`);
   }
 };
